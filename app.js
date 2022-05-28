0d5dc70e82977745382200e8acf26b604db93d22
CHiuweiyu <b07303037@ntu.edu.tw>
2022-05-28 13:25:57 +0800

    fixed

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 // parsing
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // central router
 const apiRouter = require("./routes/apiRouter");
