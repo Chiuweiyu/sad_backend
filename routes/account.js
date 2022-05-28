63af95513191ea6ea2c715b9b9f6a3f76abaaea7
ruisiang <ruisiang2000@gmail.com>
2022-05-28 18:04:00 +0800

    add account unit test and init prerequisites

--- routes/account.js
+++ routes/account.js
@@ -4,41 +4,49 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 
-const verify_login = async (req, res, next)  => {
-    let {userName, password} = req.body;
+const verify_login = async (req, res, next) => {
+    let { userName, password } = req.body;
+    if (!userName || !password) {
+        res.status(400).send('Missing parameters.');
+        return;
+    }
     let cmd = "SELECT * FROM users WHERE user_name=$1";
 
     const result = await db.query(cmd, [userName]).catch((err) => {
         throw new DatabaseError("Something Went Wrong", err);
     });
 
-    if(result.rows.length == 0){
+    if (result.rows.length == 0) {
         res.status(400).send('This account does not exist.');
     }
-    else{
-        if (bcrypt.compareSync( password, result.rows[0].pwd)){
+    else {
+        if (bcrypt.compareSync(password, result.rows[0].pwd)) {
             res.status(200).send('OK');
         }
-        else{
-          res.status(403).send('Password is not correct.');
+        else {
+            res.status(403).send('Password is not correct.');
         }
     }
 };
 
 const register = async (req, res, next) => {
-    let {userName, password} = req.body;
-    
+    let { userName, password } = req.body;
+    if (!userName || !password) {
+        res.status(400).send('Missing parameters.');
+        return;
+    }
+
     let cmd = "SELECT * FROM users WHERE user_name=$1";
     let result = await db.query(cmd, [userName]).catch((err) => {
         throw new DatabaseError("Something Went Wrong", err);
     });
 
-    if(result.rows.length != 0){
+    if (result.rows.length != 0) {
         res.status(400).send('Account exists.');
-    } 
-    else{
+    }
+    else {
         password = bcrypt.hashSync(password, saltRounds);
-        cmd = "INSERT INTO users VALUES ($1, $2, 1, 0)" ;
+        cmd = "INSERT INTO users VALUES ($1, $2, 1, 0)";
         await db.query(cmd, [userName, password]).catch((err) => {
             throw new DatabaseError("Something Went Wrong", err);
         });
