3d2de6410177295f2b4a49426228f8187dc90910
CHiuweiyu <b07303037@ntu.edu.tw>
2022-05-28 17:16:32 +0800

    size_fixed

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(compression());
 app.use(cors());
 
 // parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: false , limit: '50mb'}));
 
 // central router
 const apiRouter = require("./routes/apiRouter");
